1cae2a96c07433a88900d13d159d38389d45b321
Andrei Mataoanu <amatao1999@gmail.com>
2025-01-24 23:32:36 +0100

    Arrow updates when new destination.

--- wind-up taxi/Assets/Scripts/Arrow.cs
+++ wind-up taxi/Assets/Scripts/Arrow.cs
@@ -1,28 +1,55 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Arrow : MonoBehaviour
 {
-    public Transform destinationTransform;
     private Transform carTransform;
+    public Transform currentDestination;
+
+    private GameObject[] destinationObjects;
 
     public Vector3 arrowOffset = new Vector3(0, 1, 0);
 
     private void Start()
     {
         carTransform = GameObject.FindGameObjectWithTag("Car").GetComponent<Transform>();
+
+        destinationObjects = GameObject.FindGameObjectsWithTag("Destination");
+
+        //temp
+        currentDestination = destinationObjects[Random.Range(0, destinationObjects.Length)].transform;
     }
 
     private void Update()
     {
-        //Position the arrow above the car.
         transform.position = carTransform.position + arrowOffset;
 
-        //Make the arrow point towards the destination.
-        Vector3 direction = destinationTransform.position - transform.position;
+        Vector3 direction = currentDestination.position - transform.position;
+
         direction.y = 0;
+
         if(direction != Vector3.zero)
         {
             transform.rotation = Quaternion.LookRotation(direction);
+        }
+
+        //temp
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            SelectRandomDestination();
+        }
+    }
+
+    private void SelectRandomDestination()
+    {
+        Transform newDestination;
+
+        if(destinationObjects.Length > 0)
+        {
+            newDestination = destinationObjects[Random.Range(0, destinationObjects.Length)].transform;
+
+            currentDestination = newDestination;
         }
     }
 }
