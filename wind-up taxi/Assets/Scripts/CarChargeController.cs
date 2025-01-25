9db8fc063719727493464874aca3170d2bf549d5
Tomi <tomifaludi@emptybox.games>
2025-01-25 23:50:19 +0100

    progress on hooking up the charge behaviour to the car

--- wind-up taxi/Assets/Scripts/CarChargeController.cs
+++ wind-up taxi/Assets/Scripts/CarChargeController.cs
@@ -1,106 +1,81 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Car : MonoBehaviour
 {
     private CarStateController carStateController;
     private CarMovementController carMovementController;
-    private static PlayerInput playerInput;
+
+    private Rigidbody myRigidBody;
 
     private Coroutine chargeCoroutine;
     private Coroutine timerCoroutine;
+    private Coroutine moveCoroutine;
 
-    private float currentDistanceToTravel = 0f;
+    private float currentMaxMotorForceMultiplier = 0f;
     private float currentTimeToTravel = 0f;
-    private float distanceIncrement;
-    private float timeIncrement;
-    [SerializeField] float maxDistanceToTravel = 5f;
     [SerializeField] float maxTimeToTravel = 2f;
     [SerializeField] float maxTimeToCharge = 8f;
     [SerializeField] float maxTimeToOvercharge = 2f;
+
+    [SerializeField] bool isInCarMode = false;
 
     private void Awake()
     {
         carStateController = new CarStateController();
         carMovementController = GetComponent<CarMovementController>();
-        playerInput = new PlayerInput();
-    }
-
-    private void Start()
-    {
-        playerInput.Enable();
-        playerInput.Gameplay.Charge.started += ChargeStartInput;
-        playerInput.Gameplay.Charge.canceled += ChargeEndInput;
-
-        distanceIncrement = maxDistanceToTravel / maxTimeToCharge;
-        timeIncrement = maxTimeToTravel / maxTimeToCharge;
+        myRigidBody = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
-        // if(carStateController.GetState() == CarStateController.CarState.Moving)
-        // {
-            carMovementController.SetSteeringInput(playerInput.Gameplay.Steering.ReadValue<float>());
-        // }
+        GetInput();
+    }
+
+    private void GetInput() {
+        if(isInCarMode)
+        {
+            carMovementController.setHorizontalInput(Input.GetAxis("Horizontal"));
+            carMovementController.setVerticalInput(Input.GetAxis("Vertical"));
+            carMovementController.setBreakingInput(Input.GetKey(KeyCode.Space));
+        }
+        else
+        {
+            if(carStateController.GetState() == CarStateController.CarState.Moving)
+            {
+                carMovementController.setVerticalInput(Input.GetAxis("Vertical"));
+            }
         
-        carMovementController.SetSpeedingInput(playerInput.Gameplay.Speeding.ReadValue<float>());
-    }
+            if(Input.GetKeyDown(KeyCode.Space)
+            && carStateController.GetState() == CarStateController.CarState.Idle)
+            {
+                chargeCoroutine = StartCoroutine(ChargeBehaviour());
+            }
 
-    private void ChargeStartInput(InputAction.CallbackContext context) 
-    {
-        if(chargeCoroutine != null)
-        {
-            StopCoroutine(chargeCoroutine);
-            chargeCoroutine = null;
-        }
+            if(Input.GetKeyUp(KeyCode.Space)
+            && (carStateController.GetState() == CarStateController.CarState.Charging
+            || carStateController.GetState() == CarStateController.CarState.Overcharging))
+            {
+                if(chargeCoroutine != null)
+                {
+                    StopCoroutine(chargeCoroutine);
+                    chargeCoroutine = null;
+                }
 
-        chargeCoroutine = StartCoroutine(ChargeBehaviour());
-    }
+                if(timerCoroutine != null)
+                {
+                    StopCoroutine(timerCoroutine);
+                    timerCoroutine = null;
+                }
 
-    private void ChargeEndInput(InputAction.CallbackContext context) 
-    {
-        if(chargeCoroutine != null)
-        {
-            StopCoroutine(chargeCoroutine);
-            chargeCoroutine = null;
-        }
-
-        MoveCar();
-    }
-
-    private void MoveCar()
-    {
-        if(chargeCoroutine != null)
-        {
-            StopCoroutine(chargeCoroutine);
-            chargeCoroutine = null;
-        }
-
-        if(timerCoroutine != null)
-        {
-            StopCoroutine(timerCoroutine);
-            timerCoroutine = null;
-        }
-        
-        if(carStateController.GetState() == CarStateController.CarState.Busted)
-        {
-            Reset();
-            return;
-        }
-            
-
-        if(carStateController.GetState() == CarStateController.CarState.Charging
-        || carStateController.GetState() == CarStateController.CarState.Overcharging)
-        {
-            carStateController.SetState(CarStateController.CarState.Moving);
-            //start moving the car
+                moveCoroutine = StartCoroutine(MoveBehaviour());
+            }
         }
     }
 
     public void Reset()
     {
-        currentDistanceToTravel = 0f;
+        currentMaxMotorForceMultiplier = 0f;
         currentTimeToTravel = 0f;
 
         if(carStateController.GetState() == CarStateController.CarState.Busted)
@@ -109,7 +84,26 @@ public class Car : MonoBehaviour
         }
     }
 
-    //started when button is pressed
+    private IEnumerator MoveBehaviour()
+    {
+        carStateController.SetState(CarStateController.CarState.Moving);
+        carMovementController.modifyCurrentMotorForce(currentMaxMotorForceMultiplier);
+        carMovementController.setVerticalInput(1f);
+        
+        yield return new WaitForSeconds(currentTimeToTravel);
+
+        carMovementController.modifyCurrentMotorForce(0);
+        carMovementController.setVerticalInput(0);
+
+        yield return new WaitUntil(() => myRigidBody.linearVelocity.sqrMagnitude <= 0.1f);
+
+        carStateController.SetState(CarStateController.CarState.Idle);
+
+        myRigidBody.linearVelocity = Vector3.zero;
+        myRigidBody.angularVelocity = Vector3.zero;
+        moveCoroutine = null;
+    }
+
     private IEnumerator ChargeBehaviour()
     {
         Reset();
@@ -133,11 +127,9 @@ public class Car : MonoBehaviour
 
             //camera shake
 
-            //setting the global variables for car movement
-            currentDistanceToTravel += distanceIncrement * Time.deltaTime;
-            currentTimeToTravel += timeIncrement * Time.deltaTime;
-            
-            currentDistanceToTravel = Mathf.Min(currentDistanceToTravel, maxDistanceToTravel);
+            currentMaxMotorForceMultiplier += 1f / maxTimeToCharge * Time.deltaTime;
+            currentTimeToTravel += maxTimeToTravel / maxTimeToCharge * Time.deltaTime;
+            currentMaxMotorForceMultiplier = Mathf.Min(currentMaxMotorForceMultiplier, 1f);
             currentTimeToTravel = Mathf.Min(currentTimeToTravel, maxTimeToTravel);
 
             yield return null;
--- wind-up taxi/Assets/Scripts/CarMovementController.cs
+++ wind-up taxi/Assets/Scripts/CarMovementController.cs
@@ -3,86 +3,49 @@ using UnityEngine;
 
 public class CarMovementController : MonoBehaviour
 {
-    private float steeringInput = 0f;
-    private float speedInput = 0f;
-    [SerializeField] float motorTorque = 2000;
-    [SerializeField] float brakeTorque = 2000;
-    [SerializeField] float maxSpeed = 20;
-    [SerializeField] float steeringRange = 30;
-    [SerializeField] float steeringRangeAtMaxSpeed = 10;
-    [SerializeField] float centreOfGravityOffset = -1f;
-    [SerializeField] float antiRollForce = 20000f;
+    private float horizontalInput, verticalInput;
+    private float currentSteerAngle, currentbreakForce;
+    private bool isBreaking;
 
-    [SerializeField] WheelCollider leftRearWheel;
-    [SerializeField] WheelCollider rightRearWheel;
+    // Settings
+    [SerializeField] private float maxMotorForce, breakForce, maxSteerAngle, antiRollForce;
 
-    WheelController[] wheels;
-    Rigidbody rigidBody;
+    private float currentMotorForce;
 
-    // Start is called before the first frame update
-    void Start()
+    // Wheel Colliders
+    [SerializeField] private WheelCollider frontLeftWheelCollider, frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider, rearRightWheelCollider;
+
+    // Wheels
+    [SerializeField] private Transform frontLeftWheelTransform, frontRightWheelTransform;
+    [SerializeField] private Transform rearLeftWheelTransform, rearRightWheelTransform;
+
+    private void FixedUpdate()
     {
-        rigidBody = GetComponent<Rigidbody>();
-
-        // Adjust center of mass vertically, to help prevent the car from rolling
-        rigidBody.centerOfMass += Vector3.up * centreOfGravityOffset;
-
-        // Find all child GameObjects that have the WheelController script attached
-        wheels = GetComponentsInChildren<WheelController>();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+        ApplyAntiRoll();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    public void setHorizontalInput(float value)
     {
-        // Calculate current speed in relation to the forward direction of the car
-        // (this returns a negative number when traveling backwards)
-        float forwardSpeed = Vector3.Dot(transform.forward, rigidBody.linearVelocity);
+        horizontalInput = value;
+    }
 
-        // Calculate how close the car is to top speed
-        // as a number from zero to one
-        float speedFactor = Mathf.InverseLerp(0, maxSpeed, forwardSpeed);
-        print(speedFactor);
+    public void setVerticalInput(float value)
+    {
+        verticalInput = value;
+    }
 
-        // Use that to calculate how much torque is available 
-        // (zero torque at top speed)
-        float currentMotorTorque = Mathf.Lerp(motorTorque, 0, speedFactor);
+    public void setBreakingInput(bool value)
+    {
+        isBreaking = value;
+    }
 
-        // …and to calculate how much to steer 
-        // (the car steers more gently at top speed)
-        float currentSteerRange = Mathf.Lerp(steeringRange, steeringRangeAtMaxSpeed, speedFactor);
-
-        // Check whether the user input is in the same direction 
-        // as the car's velocity
-        bool isAccelerating = Mathf.Sign(speedInput) == Mathf.Sign(forwardSpeed);
-
-        foreach (var wheel in wheels)
-        {
-            // Apply steering to Wheel colliders that have "Steerable" enabled
-            if (wheel.steerable)
-            {
-                wheel.WheelCollider.steerAngle = steeringInput * currentSteerRange;
-            }
-            
-            if (isAccelerating)
-            {
-                // Apply torque to Wheel colliders that have "Motorized" enabled
-                if(wheel.motorized)
-                {
-                    wheel.WheelCollider.motorTorque = speedInput * currentMotorTorque;
-                }
-
-                wheel.WheelCollider.brakeTorque = 0;
-            }
-            else
-            {
-                // If the user is trying to go in the opposite direction
-                // apply brakes to all wheels
-                wheel.WheelCollider.brakeTorque = Mathf.Abs(speedInput) * brakeTorque;
-                wheel.WheelCollider.motorTorque = 0;
-            }
-        }
-
-        ApplyAntiRoll();
+    public void modifyCurrentMotorForce(float value)
+    {
+        currentMotorForce = maxMotorForce * value;
     }
 
     public void ApplyAntiRoll()
@@ -90,25 +53,50 @@ public class CarMovementController : MonoBehaviour
         WheelHit hitLeft, hitRight;
         float travelLeft = 1f, travelRight = 1f;
 
-        if(leftRearWheel.GetGroundHit(out hitLeft))
-            travelLeft = (-leftRearWheel.transform.InverseTransformPoint(hitLeft.point).y - leftRearWheel.radius) / leftRearWheel.suspensionDistance;
-        if(rightRearWheel.GetGroundHit(out hitRight))
-            travelRight = (-rightRearWheel.transform.InverseTransformPoint(hitRight.point).y - rightRearWheel.radius) / rightRearWheel.suspensionDistance;
+        if (rearLeftWheelCollider.GetGroundHit(out hitLeft))
+            travelLeft = (-rearLeftWheelCollider.transform.InverseTransformPoint(hitLeft.point).y - rearLeftWheelCollider.radius) / rearLeftWheelCollider.suspensionDistance;
+        if (rearRightWheelCollider.GetGroundHit(out hitRight))
+            travelRight = (-rearRightWheelCollider.transform.InverseTransformPoint(hitRight.point).y - rearRightWheelCollider.radius) / rearRightWheelCollider.suspensionDistance;
 
         float force = (travelLeft - travelRight) * antiRollForce;
-        if(leftRearWheel.isGrounded)
-            GetComponent<Rigidbody>().AddForceAtPosition(leftRearWheel.transform.up * -force, leftRearWheel.transform.position);
-        if(rightRearWheel.isGrounded)
-            GetComponent<Rigidbody>().AddForceAtPosition(rightRearWheel.transform.up * force, rightRearWheel.transform.position);
+        if (rearLeftWheelCollider.isGrounded)
+            GetComponent<Rigidbody>().AddForceAtPosition(rearLeftWheelCollider.transform.up * -force, rearLeftWheelCollider.transform.position);
+        if (rearRightWheelCollider.isGrounded)
+            GetComponent<Rigidbody>().AddForceAtPosition(rearRightWheelCollider.transform.up * force, rearRightWheelCollider.transform.position);
     }
 
-    public void SetSteeringInput(float value)
-    {
-        steeringInput = value;
+    private void HandleMotor() {
+        frontLeftWheelCollider.motorTorque = verticalInput * currentMotorForce;
+        frontRightWheelCollider.motorTorque = verticalInput * currentMotorForce;
+        currentbreakForce = isBreaking ? breakForce : 0f;
+        ApplyBreaking();
     }
 
-    public void SetSpeedingInput(float value)
-    {
-        speedInput = value;
+    private void ApplyBreaking() {
+        frontRightWheelCollider.brakeTorque = currentbreakForce;
+        frontLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearRightWheelCollider.brakeTorque = currentbreakForce;
+    }
+
+    private void HandleSteering() {
+        currentSteerAngle = maxSteerAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRightWheelCollider.steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateWheels() {
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform) {
+        Vector3 pos;
+        Quaternion rot; 
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
     }
 }
