f280b4fa2c776865b4f6a6d141a193e3c8bde3d1
Tomi <tomifaludi@emptybox.games>
2025-01-26 18:11:19 +0100

    made destination range outline

---
+++ wind-up taxi/Assets/Scripts/DestinationRangeGizmo.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class DestinationRangeGizmo : MonoBehaviour
+{
+    private GameObject rangeIndicator;
+    private SphereCollider sphereCollider;
+    [SerializeField] private Material rangeIndicatorMaterial;
+
+    private void Awake()
+    {
+        sphereCollider = GetComponent<SphereCollider>();
+    }
+
+    private void Start()
+    {
+        rangeIndicator = CreateRangeIndicator(sphereCollider.radius);
+    }
+
+    // private void FixedUpdate()
+    // {
+    //     rangeIndicator.transform.position = new Vector3(transform.position.x, 0.21f, transform.position.z);
+    // }
+
+    private void OnDisable()
+    {
+        rangeIndicator.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        rangeIndicator.SetActive(true);
+    }
+
+    private GameObject CreateRangeIndicator(float range)
+    {
+        GameObject indicator = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        // indicator.transform.SetParent(transform);
+        Destroy(indicator.GetComponent<Collider>());
+        indicator.transform.localScale = new Vector3(range * 2, 0.01f, range * 2);
+        indicator.GetComponent<Renderer>().material = rangeIndicatorMaterial;
+        indicator.transform.position = new Vector3(transform.position.x, 0.21f, transform.position.z); 
+        return indicator;
+    }
+}
