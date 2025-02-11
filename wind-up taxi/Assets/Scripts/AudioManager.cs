88e817379da25a0f22e2707e3d308e07fb9e3d93
Mikkel Rasmussen <Rasmussen.Mikkel@outlook.dk>
2025-02-11 11:07:07 +0100

    reorganized event folder structure in FMOD
    and changed paths in unity appropriately

--- wind-up taxi/Assets/Scripts/AudioManager.cs
+++ wind-up taxi/Assets/Scripts/AudioManager.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using FMODUnity;
+using FMOD.Studio;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance { get; private set; }
+
+    private List<EventInstance> eventInstances;
+
+    private List<StudioEventEmitter> eventEmitters;
 
     private void Awake()
     {
@@ -12,10 +19,50 @@ public class AudioManager : MonoBehaviour
             Debug.LogError("Found more than one AudioManager in the scene.");
         }
         instance = this;
+
+        eventInstances= new List<EventInstance>();
+        eventEmitters= new List<StudioEventEmitter>();
     }
 
     public void PlayOneShot(EventReference sound, Vector3 worldPos)
     {
         RuntimeManager.PlayOneShot(sound, worldPos);
     }
+
+    public EventInstance CreateEventInstance(EventReference eventReference)
+    {
+        EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        eventInstances.Add(eventInstance);
+        return eventInstance;
+    }
+
+    public StudioEventEmitter InitializeEventEmitter(EventReference eventReference, GameObject emitterGameObject)
+    {
+        StudioEventEmitter emitter = emitterGameObject.GetComponent<StudioEventEmitter>();
+        emitter.EventReference = eventReference;
+        eventEmitters.Add(emitter);
+        return emitter;
+    }
+
+    private void CleanUp()
+    {
+        // stop and release any created instances
+        foreach (EventInstance eventInstance in eventInstances)
+        {
+            eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            eventInstance.release();
+        }
+
+        // stop all of the event emitters, because if we don't, they may hang around in other scenes
+        foreach (StudioEventEmitter emitter in eventEmitters)
+        {
+            emitter.Stop();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        CleanUp();
+    }
+
 }
--- wind-up taxi/Assets/Scripts/FMODEvents.cs
+++ wind-up taxi/Assets/Scripts/FMODEvents.cs
@@ -5,15 +5,29 @@ public class FMODEvents : MonoBehaviour
 {
     public static FMODEvents instance { get; private set; }
 
+    [field: Header("Ambience")]
+    [field: SerializeField] public EventReference city { get; private set; }
 
-    [field: Header("Car SFX")]
+    [field: Header("Music")]
+    [field: SerializeField] public EventReference taxiMusic { get; private set; }
 
+
+
+    [field: Header("Player SFX")]
     [field: SerializeField] public EventReference carHit { get; private set; }
+    [field: SerializeField] public EventReference carHonk { get; private set; }
+    [field: SerializeField] public EventReference carEngine { get; private set; }
+    [field: SerializeField] public EventReference carWindUp { get; private set; }
 
 
-    [field: Header("Robot SFX")]
-
+    [field: Header("NPC SFX")]
     [field: SerializeField] public EventReference robotDeathScream { get; private set; }
+    [field: SerializeField] public EventReference npcHonk { get; private set; }
+
+
+    [field: Header("UI")]
+    [field: SerializeField] public EventReference pickUp { get; private set; }
+    [field: SerializeField] public EventReference dropOff { get; private set; }
 
     private void Awake()
     {
